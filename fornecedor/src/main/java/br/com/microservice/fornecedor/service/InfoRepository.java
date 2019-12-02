7a7ddb8529b40440529037977f87e54e046faeb4
Luiz Martins <43974104+LuizOMartins@users.noreply.github.com>
2019-12-01 23:42:20 -0300

    Microsserviço
    Rest Template
    Service discovery
    Service Registry
    Eureka

---
+++ fornecedor/src/main/java/br/com/microservice/fornecedor/service/InfoRepository.java
@@ -0,0 +1,13 @@
+package br.com.microservice.fornecedor.service;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.microservice.fornecedor.model.InfoFornecedor;
+
+@Repository
+public interface InfoRepository extends CrudRepository<InfoFornecedor, Long> {
+	
+	InfoFornecedor findByEstado(String estado); //implementado pelo proprio spring data baseado no nome do metodo
+
+}
---
+++ loja/src/main/java/br/com/microservice/loja/controller/CompraController.java
@@ -0,0 +1,26 @@
+package br.com.microservice.loja.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.microservice.loja.controller.dto.CompraDTO;
+import br.com.microservice.loja.service.CompraService;
+
+@RestController
+@RequestMapping("/compra")
+public class CompraController {
+	
+	
+	@Autowired 
+	private CompraService compraService;//processar a compra	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void realicaCompra(@RequestBody CompraDTO compra) {
+		compraService.realizaCompra(compra);
+			
+	}
+
+}
